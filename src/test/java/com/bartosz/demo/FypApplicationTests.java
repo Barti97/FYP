5e267928a0ad1715a2cb5ea9a36891707b9d4158
Bartosz Hofman <bartoszhofman97@gmail.com>
2020-02-02 16:14:40 +0000

    Initial Commit

---
+++ src/test/java/com/bartosz/demo/FypApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartosz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FypApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
